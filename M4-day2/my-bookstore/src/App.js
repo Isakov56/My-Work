181a1db6f825468ac80941ca0f30c87911e75960
isakov <isakov56cc@gmail.com>
2020-11-17 20:52:48 +0100

    react-app

---
+++ M4-day2/my-bookstore/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import NavBar from "./Components/NavBar"
+//import Jumbotron from "./Components/Jumbotron"
+
+function App() {
+  return (
+    <>
+     <NavBar />
+
+    </>
+  );
+}
+
+export default App;
---
+++ M4-day2/my-bookstore/src/Components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import {Navbar, Nav, Form, FormControl, Button} from "react-bootstrap"
+
+const NavBar= () => {
+    return <div>
+        
+    </div>
+}
+
+export default NavBar
